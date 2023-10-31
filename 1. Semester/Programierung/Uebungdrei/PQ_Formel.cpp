03fca36f1ad1d9529c7c1ca98626156231ca67cd
Julian Bertol <122291185+JulianBertol@users.noreply.github.com>
2023-10-31 14:31:33 +0100

    Update zu Aufgabe 3

--- 1. Semester/Programierung/Uebungdrei/PQ_Formel.cpp
+++ 1. Semester/Programierung/Uebungdrei/PQ_Formel.cpp
@@ -1,11 +1,28 @@
 #include<stdio.h>
+#include<math.h>
 
 int main(){
-    int p;
-    int q;
+    double p;
+    double q;
+    double rechnung_pos; //Rechnung für x1
+    double rechnung_neg; //Rechnung für x2
+    double rechnung_ges; //Berechnungen die beide brauchen
 
-    printf("Geben sie eine Zahl für p ein:\n");
-    scanf("%i",&p);
-    printf("Geben sie eine Zahl für q ein:\n");
-    scanf("%i",&q);
-}
+    printf("Zahl für p eingeben: \n");
+    scanf("%lf", &p);
+    printf("Zahl fpr q eingeben: \n");
+    scanf("%lf", &q);
+
+    printf("p = %lf\n q = %lf\n", p,q);
+    rechnung_ges = (p/2)*(p/2)-q;
+    if (rechnung_ges >= 0){
+        rechnung_pos = -p/2 + sqrt(rechnung_ges);
+        rechnung_neg = -p/2 - sqrt(rechnung_ges);
+        printf("X1 = %lf\n", rechnung_pos);
+        printf("X2 = %lf",rechnung_neg);
+    }
+    else{
+        printf("Rechung nicht möglich, da die wurzel negativ ist (%lf)", rechnung_ges);
+    };
+    return 0;
+    }
---
+++ 1. Semester/Programierung/Uebungdrei/Tage_pro_monat.cpp
@@ -0,0 +1,2 @@
+#include <stdio.h>
+//comming soon
