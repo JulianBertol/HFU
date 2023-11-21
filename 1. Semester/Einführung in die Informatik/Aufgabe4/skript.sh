1780c810873a2f42e3563f2a08cb0cf10e175756
Julian Bertol <122291185+JulianBertol@users.noreply.github.com>
2023-11-21 12:09:03 +0100

    fix bugs

--- 1. Semester/Einführung in die Informatik/Aufgabe4/skript.sh
+++ 1. Semester/Einführung in die Informatik/Aufgabe4/skript.sh
@@ -1,2 +1,18 @@
-./Matplotlib/main.py
-xdg-open Matplotlib/funktion.png
+#!/bin/bash
+
+# Virtual Environment erstellen
+python3 -m venv venv
+
+# Virtual Environment aktivieren
+source venv/bin/activate
+
+
+pip install -r requirements.txt
+
+
+python Matplotlib/main.py
+
+open funktion.png
+# Virtual Environment deaktivieren
+deactivate
+
