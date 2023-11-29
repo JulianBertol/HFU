413de1dcffe395bbfe03a3a47762228cd253b165
Julian Bertol <122291185+JulianBertol@users.noreply.github.com>
2023-11-29 17:10:19 +0100

    Fix bugs

--- 1. Semester/Programierung/Aufgabenblatt7/rechner.cpp
+++ 1. Semester/Programierung/Aufgabenblatt7/rechner.cpp
@@ -5,7 +5,7 @@ int berechne(char op, int v1, int v2){
     int rechnung;
     switch (op)
     {
-    case '*':
+    case 'x':
         rechnung = v1 * v2;
         break;
 
@@ -31,11 +31,18 @@ int berechne(char op, int v1, int v2){
 
 int main (int argc, char* argv[]){
     argc --;
+    if (argc == 0){
+        printf("Du hast nicht Uebergeben!\n");
+        return 0;
+    }
     if (argc > 3){
         printf("Du hast zu viel eingegeben! \n");
+    }else if (argc < 3 ){
+        printf("Du hast zu wenig eingegeben!\n");
     }else if(!atoi(argv[2]) || !atoi(argv[3])){
         printf("Du hast keine Zahl eingegeben!\n");
-    }else{
+    }
+    else{
     printf("Operator: %c \n", *argv[1]);
     printf("erster int: %i\n", atoi(argv[2]));
     printf("zweiter int: %i\n", atoi(argv[3]));
