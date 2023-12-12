0d3ca91f9f8bdd497ad3ac030945a7a48aed357b
Whitecyberboy <122291185+Whitecyberboy@users.noreply.github.com>
2023-12-12 11:49:41 +0100

    add Aufgabe 5

---
+++ 1. Semester/Einführung in die Informatik/Aufgabe5/main.py
@@ -0,0 +1,23 @@
+def task1(a, b):
+    return not a and not b
+
+def task2(a, c):
+    return a or not c
+
+def task3():
+    return True
+
+def task4(a):
+    return a or b and c
+
+def task5(a ,b):
+    return a and not b
+
+def task6(a, c):
+    return not b and (d or c)
+
+def task7(a, b, c):
+    return False
+
+def task8(a, b, c):
+    return (a and b) and not c
