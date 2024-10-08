c6127d5cb1d7d7cf28d5514f03f4e76a556a3746
Julian Bertol <122291185+JulianBertol@users.noreply.github.com>
2024-10-08 16:24:48 +0200

    fix bugs

--- 3.Semester/Objektorientiertes Programieren/Worksheet 1/Task3/main.cpp
+++ 3.Semester/Objektorientiertes Programieren/Worksheet 1/Task3/main.cpp
@@ -11,21 +11,32 @@ bool toInt(char c, int *theInt) {
 }
 
 int decode(const char *line) {
-    int temp;
-    int result = 0;
+    int firstnumber = 0, lastnumber = 0, digit = 0;
+    bool found = false;
     for (int i = 0; line[i] != '\0'; ++i) {
-        if (toInt(line[i], &temp)) {
-            result = result * 10 + temp;
+        if (toInt(line[i], &digit)) {
+            if (!found) {
+                firstnumber = digit;
+                found = true;
+            }else
+                lastnumber = digit;
         }
     }
-    return result;
+
+    if (!found)
+        throw std::invalid_argument("Invalid input");
+    if (!lastnumber)
+        return firstnumber;
+
+    return firstnumber * 10 + lastnumber;
 }
+
 
 int main() {
     std::cout << "Starting ..." << std::endl;
     assert(decode("1xyz2") == 12);
     assert(decode("abc3pqr8ijk") == 38);
-    assert(decode("x1y2z3v4w5q") == 12345);
+    assert(decode("x1y2z3v4w5q") == 15);
     assert(decode("hello7world") == 7);
     std::cout << "Finished ..." << std::endl;
     return 0;
