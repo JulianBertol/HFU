b10b9371c6ef3928497337d94376d931c5ec1956
Julian Bertol <122291185+JulianBertol@users.noreply.github.com>
2024-11-07 11:32:18 +0100

    Finish Task 4

--- 3.Semester/Objektorientiertes Programieren/Worksheet 4/City.cpp
+++ 3.Semester/Objektorientiertes Programieren/Worksheet 4/City.cpp
@@ -1,5 +1,49 @@
-//
-// Created by Julian on 29.10.2024.
-//
-
 #include "City.h"
+
+#include <stdexcept>
+
+hfu::City::City(const Position& position, const std::string* POI, const int number_of_POI)
+        :position(position), number_of_POI(number_of_POI), POI(initializepois(POI, number_of_POI)){
+    };
+
+    hfu::City::~City() {
+        delete[] POI;
+    }
+
+    std::string* hfu::City::initializepois(const std::string* POIS, const int length) {
+        std::string* POI = new std::string[length];
+        for (int i = 0; i < length; i++) {
+            POI[i] = POIS[i];
+        }
+        return POI;
+    }
+
+    std::string hfu::City::getName() const {
+        return this->position.getName();
+    }
+
+    int hfu::City::getX() const {
+        return this->position.getX();
+    }
+
+    int hfu::City::getY() const {
+        return this->position.getY();
+    }
+
+    std::string hfu::City::getPOI(const int index) const {
+        if (index >= 0 && index < number_of_POI) {
+            return POI[index];
+        }
+        throw std::out_of_range("Index out of range");
+    }
+
+    void hfu::City::setPOI(const int i, const std::string &POI) const {
+        if(i >= 0 && i < number_of_POI - 1) {
+            this->POI[i] = POI;
+        }else
+            throw std::out_of_range("Index out of range");
+    }
+
+    int hfu::City::getNumberOfPOI() const {
+        return this->number_of_POI;
+    }
--- 3.Semester/Objektorientiertes Programieren/Worksheet 4/City.h
+++ 3.Semester/Objektorientiertes Programieren/Worksheet 4/City.h
@@ -1,16 +1,29 @@
-//
-// Created by Julian on 29.10.2024.
-//
-
 #ifndef CITY_H
 #define CITY_H
+#include <string>
+#include "Position.h"
+
+
+namespace hfu {
+    class City {
+        private:
+        Position position;
+        std::string *POI;
+        int number_of_POI;
+        std::string *initializepois(const std::string *POIS, int length);
+
+        public:
+        City(const Position& position, const std::string* POI, int number_of_POI);
+        ~City();
+        std::string getName() const;
+        int getX() const;
+        int getY() const;
+        std::string getPOI(int index) const;
+        void setPOI(int i, const std::string& POI) const;
+        int getNumberOfPOI() const;
+    };
+}
 
 
 
-class City {
-
-};
-
-
-
-#endif //CITY_H
+#endif
